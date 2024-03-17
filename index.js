4563734ca21116a9f7f8d83432347e070260294a
DaveSalterM <davesalter1988@gmail.com>
2024-03-17 14:28:53 -0700

    index inputs

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please provide the title of your new project.'
+        },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining the what, why, and how of your project.'
+        },
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?'
+        },
+    
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use.'
+        },
 
-// Function call to initialize app
-init();
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List your collaborators, if any, with links to their GitHub profiles.'
+        },
+    
+        {
+            type: 'list',
+            name: 'license',
+            message: 'List the license used for this project',
+            choices: ['Apache', 'GNU GPL v2.0', 'GNU GPL v3.0', 'MIT', 'BSD 2-Clause', 'BSD 3-Clause', 'Boost Software', 'Creative Commons Zero', 'Eclipse', 'Mozilla', 'Unlicense',]
+        },
+
+        {
+            type: 'input',
+            name: 'features',
+            message: 'If your project has a lot of features, list them here.'
+        },
+
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Tests for application'
+        },
+        
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Provide your GitHub username'
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Provide your E-Mail address for addition user questions'
+        },
+    ])
+
